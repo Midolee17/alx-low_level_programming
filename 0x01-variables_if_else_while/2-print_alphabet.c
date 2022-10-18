9d90b95f54cd7873695bc4bacf9a3d90c88b40e1
Midolee17 <tiamiyuhammed17@gmail.com>
2022-10-17 18:25:19 -0700

    Okay

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - lower case alpha
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
